1b916bae2862c6ac644d092cc86febf9921ad524
Mirjalol49 <mirjalolxurshida4941@gmail.com>
2023-10-13 04:15:19 -0700

    Video is fixed

--- src/Components/Home/HeroBox/index.jsx
+++ src/Components/Home/HeroBox/index.jsx
@@ -2,14 +2,19 @@ import React from "react";
 import "./index.css";
 import Header from "../../Header";
 import Hero from "../Hero";
-import videoSrc from "../../../assets/video/IT_Park_в_Узбекистане_Кто_развивает_IT_экспорт_в_Центральной_Азии.mp4";
 
 const HeroBox = () => {
   return (
     <div className="background">
-      <video autoPlay muted loop className="video-background">
-        <source src={videoSrc} type="video/mp4" />
-      </video>
+      <iframe
+        title="IT Park в Узбекистане. Кто развивает IT- экспорт в Центральной Азии"
+        width="150%"
+        height="150%"
+        src="https://www.youtube.com/embed/v-9LW78-JPs?autoplay=1&loop=1&mute=1&controls=0&rel=0&modestbranding=1&showinfo=0&autohide=1&cc_load_policy=3&fs=1"
+        frameBorder="0"
+        allowFullScreen
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      ></iframe>
       <div className="content-overlay">
         <Header />
         <Hero />
